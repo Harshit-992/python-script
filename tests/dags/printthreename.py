8a1ef1f07499c7f8d0b18c251f720abd6324b486
Harshit-992 <85869265+Harshit-992@users.noreply.github.com>
2023-10-20 15:52:02 +0530

    Create printthreename.py

---
+++ tests/dags/printthreename.py
@@ -0,0 +1,42 @@
+from airflow import DAG
+from airflow.operators.python_operator import PythonOperator
+from datetime import datetime
+
+# Define default_args with information about the DAG, such as owner, start_date, and schedule_interval.
+default_args = {
+    'owner': 'your_name',
+    'start_date': datetime(2023, 1, 1),
+    'retries': 1,  # Number of retries upon failure
+}
+
+# Create a DAG instance with a unique ID and default_args.
+with DAG('print_name_dag', default_args=default_args, schedule_interval=None, catchup=False) as dag:
+
+    # Define three PythonOperator tasks, each printing a name.
+    def print_name_1():
+        print("Task 1: John")
+
+    def print_name_2():
+        print("Task 2: Jane")
+
+    def print_name_3():
+        print("Task 3: Alice")
+
+    # Create three PythonOperator tasks.
+    print_name_task_1 = PythonOperator(
+        task_id='print_name_task_1',
+        python_callable=print_name_1
+    )
+
+    print_name_task_2 = PythonOperator(
+        task_id='print_name_task_2',
+        python_callable=print_name_2
+    )
+
+    print_name_task_3 = PythonOperator(
+        task_id='print_name_task_3',
+        python_callable=print_name_3
+    )
+
+# Define the task order: the three tasks should run sequentially.
+print_name_task_1 >> print_name_task_2 >> print_name_task_3
