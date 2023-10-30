ac8a0c47414e52ff41c0665ad6f6ff24bf1210b4
Harshit-992 <85869265+Harshit-992@users.noreply.github.com>
2023-10-30 12:34:36 +0530

    Update input.py

--- tests/dags/input.py
+++ tests/dags/input.py
@@ -1,35 +1,22 @@
 from airflow import DAG
 from airflow.operators.python_operator import PythonOperator
 from airflow.utils.dates import days_ago
-from airflow.models import Variable
 
-# Define DAG
-dag = DAG(
+# Define a Python function to print the name
+def print_name(**kwargs):
+    name = kwargs['dag_run'].conf.get('name')
+    print(f"Hello, {name}!")
+
+# Define the DAG
+with DAG(
     'input_name_example',
     schedule_interval=None,  # This DAG is not scheduled
     start_date=days_ago(1),
     catchup=False,
-)
-
-# Python function to perform a task using parameters
-def multiply_numbers(**kwargs):
-    # Get the values of 'number1' and 'number2' from Variables
-    number1 = Variable.get("number1")
-    number2 = Variable.get("number2")
-
-    # Perform the task (multiply numbers)
-    result = int(number1) * int(number2)
-
-    # Print the result
-    print(f"The result of {number1} * {number2} is {result}")
-
-# Create a PythonOperator that uses parameters
-multiply_task = PythonOperator(
-    task_id='multiply_task',
-    python_callable=multiply_numbers,
-    provide_context=True,  # Pass the context to the function
-    dag=dag,
-)
-
-# Set the task dependencies (no dependencies in this case)
-multiply_task
+) as dag:
+    # Create a PythonOperator task to run the print_name function
+    print_name_task = PythonOperator(
+        task_id='print_name_task',
+        python_callable=print_name,
+        provide_context=True,  # Pass the context to the function
+    )
