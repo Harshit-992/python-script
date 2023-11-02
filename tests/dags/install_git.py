098bb79dd19b5171a5f2f88ec2dab81774cd41f0
Harshit-992 <85869265+Harshit-992@users.noreply.github.com>
2023-11-02 13:14:28 +0530

    Create install_git.py

---
+++ tests/dags/install_git.py
@@ -0,0 +1,28 @@
+from airflow import DAG
+from airflow.operators.bash_operator import BashOperator
+from airflow.utils.dates import days_ago
+
+default_args = {
+    'owner': 'airflow',
+    'start_date': days_ago(1),
+    'depends_on_past': False,
+    'retries': 1,
+}
+
+dag = DAG(
+    'install_git',
+    default_args=default_args,
+    description='Install Git using Airflow',
+    schedule_interval=None,  # This DAG is not scheduled
+    catchup=False,
+)
+
+# Use a BashOperator to install git
+install_git_task = BashOperator(
+    task_id='install_git',
+    bash_command='apt-get update && apt-get install -y git',  # For Debian/Ubuntu systems
+    dag=dag,
+)
+
+# Set the task dependencies
+install_git_task
