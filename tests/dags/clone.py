d35c3150ae2be82d61db249ee2c2868cb9ca18ce
Harshit-992 <85869265+Harshit-992@users.noreply.github.com>
2023-11-09 11:26:58 +0530

    Update clone.py

--- tests/dags/clone.py
+++ tests/dags/clone.py
@@ -26,106 +26,106 @@ folder_path = '~/clone/ssh/'
 
 ssh_key = Variable.get("ssh_key")
 
-# build_jar = BashOperator(
-#     task_id='build_jar',
-#     bash_command=f"""pwd
-#                      mkdir -p {folder_path}
-#                      echo "{ssh_key}" > {folder_path}ssh_key
-#                      ls {folder_path}
-#                      cat {folder_path}ssh_key
-#                      ls {folder_path}
-#                      mkdir -p /home/airflow/.ssh
-#                      touch /home/airflow/.ssh/known_hosts
-#                      ssh-keyscan gitlab.intelligrape.net >> ~/.ssh/known_hosts
-#                      chmod 600 {folder_path}ssh_key
-#                      ssh-agent bash -c 'ssh-add {folder_path}ssh_key; git clone git@gitlab.intelligrape.net:tothenew/ckdataprocessengine.git '
-#                      git checkout prod-merge-CKPIP-28
-#                      ls 
-#                      cd ckdataprocessengine
-#                      git branch
-#                      git checkout prod-merge-CKPIP-28
-#                      git branch
-#                      /home/airflow/.sdkman/candidates/sbt/1.9.7/bin/sbt about
-#                      /home/airflow/.sdkman/candidates/sbt/1.9.7/bin/sbt clean
-#                      /home/airflow/.sdkman/candidates/sbt/1.9.7/bin/sbt assembly
-#                      aws s3 cp target/scala-2.12/CkDataProcessEngine-assembly-0.1.jar  s3://ck-data-pipeline-auto-demo-config-prod-data/spark_utility/91/
-#                      ls
+build_jar = BashOperator(
+    task_id='build_jar',
+    bash_command=f"""pwd
+                     mkdir -p {folder_path}
+                     echo "{ssh_key}" > {folder_path}ssh_key
+                     ls {folder_path}
+                     cat {folder_path}ssh_key
+                     ls {folder_path}
+                     mkdir -p /home/airflow/.ssh
+                     touch /home/airflow/.ssh/known_hosts
+                     ssh-keyscan gitlab.intelligrape.net >> ~/.ssh/known_hosts
+                     chmod 600 {folder_path}ssh_key
+                     ssh-agent bash -c 'ssh-add {folder_path}ssh_key; git clone git@gitlab.intelligrape.net:tothenew/ckdataprocessengine.git '
+                     git checkout prod-merge-CKPIP-28
+                     ls 
+                     cd ckdataprocessengine
+                     git branch
+                     git checkout prod-merge-CKPIP-28
+                     git branch
+                     /home/airflow/.sdkman/candidates/sbt/1.9.7/bin/sbt about
+                     /home/airflow/.sdkman/candidates/sbt/1.9.7/bin/sbt clean
+                     /home/airflow/.sdkman/candidates/sbt/1.9.7/bin/sbt assembly
+                     aws s3 cp target/scala-2.12/CkDataProcessEngine-assembly-0.1.jar  s3://ck-data-pipeline-auto-demo-config-prod-data/spark_utility/92/
+                     ls
                      
                                          
 
-#     """,
-#     dag=dag,
-# )
+    """,
+    dag=dag,
+)
 
-# ri_config = BashOperator(
-#     task_id='ri_config',
-#     bash_command=f"""pwd
-#                      mkdir -p {folder_path}
-#                      echo "{ssh_key}" > {folder_path}ssh_key
-#                      ls {folder_path}
-#                      cat {folder_path}ssh_key
-#                      ls {folder_path}
-#                      mkdir -p /home/airflow/.ssh
-#                      touch /home/airflow/.ssh/known_hosts
-#                      ssh-keyscan gitlab.intelligrape.net >> ~/.ssh/known_hosts
-#                      chmod 600 {folder_path}ssh_key
-#                      ssh-agent bash -c 'ssh-add {folder_path}ssh_key; git clone git@gitlab.intelligrape.net:tothenew/mycloud-scripts.git '
-#                      ls 
-#                      cd mycloud-scripts
-#                      ls
-#                      git checkout ck-data-pipeline-auto-demo-uat
-#                      python3 optimized_config/auto_ri_config.py 2023 10                     
+ri_config = BashOperator(
+    task_id='ri_config',
+    bash_command=f"""pwd
+                     mkdir -p {folder_path}
+                     echo "{ssh_key}" > {folder_path}ssh_key
+                     ls {folder_path}
+                     cat {folder_path}ssh_key
+                     ls {folder_path}
+                     mkdir -p /home/airflow/.ssh
+                     touch /home/airflow/.ssh/known_hosts
+                     ssh-keyscan gitlab.intelligrape.net >> ~/.ssh/known_hosts
+                     chmod 600 {folder_path}ssh_key
+                     ssh-agent bash -c 'ssh-add {folder_path}ssh_key; git clone git@gitlab.intelligrape.net:tothenew/mycloud-scripts.git '
+                     ls 
+                     cd mycloud-scripts
+                     ls
+                     git checkout ck-data-pipeline-auto-demo-uat
+                     python3 optimized_config/auto_ri_config.py 2023 10                     
 
-#     """,
-#     dag=dag,
-# )
-# refresh_data = BashOperator(
-#     task_id='refresh_data',
-#     bash_command=f"""pwd
-#                      mkdir -p {folder_path}
-#                      echo "{ssh_key}" > {folder_path}ssh_key
-#                      ls {folder_path}
-#                      cat {folder_path}ssh_key
-#                      ls {folder_path}
-#                      mkdir -p /home/airflow/.ssh
-#                      touch /home/airflow/.ssh/known_hosts
-#                      ssh-keyscan gitlab.intelligrape.net >> ~/.ssh/known_hosts
-#                      chmod 600 {folder_path}ssh_key
-#                      ssh-agent bash -c 'ssh-add {folder_path}ssh_key; git clone git@gitlab.intelligrape.net:tothenew/mycloud-scripts.git '
-#                      ls 
-#                      cd mycloud-scripts
-#                      ls
-#                      git checkout ck-master-refresh-emr-pipeline-auto-demo
-#                      python3 ck_auto_demo_emr/buckets_json.py
-#                      python3 ck_auto_demo_emr/main.py --year 2023 --month 10 --app ck-auto-demo --env prod --build_number 91 --flow refresh-data --template payer --payer '674600239845,741843927392' --core_node_spot_percent 80
+    """,
+    dag=dag,
+)
+refresh_data = BashOperator(
+    task_id='refresh_data',
+    bash_command=f"""pwd
+                     mkdir -p {folder_path}
+                     echo "{ssh_key}" > {folder_path}ssh_key
+                     ls {folder_path}
+                     cat {folder_path}ssh_key
+                     ls {folder_path}
+                     mkdir -p /home/airflow/.ssh
+                     touch /home/airflow/.ssh/known_hosts
+                     ssh-keyscan gitlab.intelligrape.net >> ~/.ssh/known_hosts
+                     chmod 600 {folder_path}ssh_key
+                     ssh-agent bash -c 'ssh-add {folder_path}ssh_key; git clone git@gitlab.intelligrape.net:tothenew/mycloud-scripts.git '
+                     ls 
+                     cd mycloud-scripts
+                     ls
+                     git checkout ck-master-refresh-emr-pipeline-auto-demo
+                     python3 ck_auto_demo_emr/buckets_json.py
+                     python3 ck_auto_demo_emr/main.py --year 2023 --month 10 --app ck-auto-demo --env prod --build_number 92 --flow refresh-data --template payer --payer '674600239845,741843927392' --core_node_spot_percent 80
 
-#     """,
-#     dag=dag,
-# )
-# emr_process = BashOperator(
-#     task_id='emr_process',
-#     bash_command=f"""pwd
-#                      mkdir -p {folder_path}
-#                      echo "{ssh_key}" > {folder_path}ssh_key
-#                      ls {folder_path}
-#                      cat {folder_path}ssh_key
-#                      ls {folder_path}
-#                      mkdir -p /home/airflow/.ssh
-#                      touch /home/airflow/.ssh/known_hosts
-#                      ssh-keyscan gitlab.intelligrape.net >> ~/.ssh/known_hosts
-#                      chmod 600 {folder_path}ssh_key
-#                      ssh-agent bash -c 'ssh-add {folder_path}ssh_key; git clone git@gitlab.intelligrape.net:tothenew/mycloud-scripts.git '
-#                      ls 
-#                      cd mycloud-scripts
-#                      ls
-#                      git checkout ck-master-refresh-emr-pipeline-auto-demo
-#                      python3 ck_auto_demo_emr/buckets_json.py
-#                      python3 ck_auto_demo_emr/main.py --year 2023 --month 10 --app ck-auto-demo --env prod --build_number 91 --flow process-data --template payer --payer '674600239845,741843927392' --core_node_spot_percent 80
-#                      python3 ck_auto_demo_emr/main.py --year 2023 --month 10 --app ck-auto-demo --env prod --build_number 91 --flow terminate-cluster --template payer --payer '674600239845,741843927392' --core_node_spot_percent 80
+    """,
+    dag=dag,
+)
+emr_process = BashOperator(
+    task_id='emr_process',
+    bash_command=f"""pwd
+                     mkdir -p {folder_path}
+                     echo "{ssh_key}" > {folder_path}ssh_key
+                     ls {folder_path}
+                     cat {folder_path}ssh_key
+                     ls {folder_path}
+                     mkdir -p /home/airflow/.ssh
+                     touch /home/airflow/.ssh/known_hosts
+                     ssh-keyscan gitlab.intelligrape.net >> ~/.ssh/known_hosts
+                     chmod 600 {folder_path}ssh_key
+                     ssh-agent bash -c 'ssh-add {folder_path}ssh_key; git clone git@gitlab.intelligrape.net:tothenew/mycloud-scripts.git '
+                     ls 
+                     cd mycloud-scripts
+                     ls
+                     git checkout ck-master-refresh-emr-pipeline-auto-demo
+                     python3 ck_auto_demo_emr/buckets_json.py
+                     python3 ck_auto_demo_emr/main.py --year 2023 --month 10 --app ck-auto-demo --env prod --build_number 92 --flow process-data --template payer --payer '674600239845,741843927392' --core_node_spot_percent 80
+                     python3 ck_auto_demo_emr/main.py --year 2023 --month 10 --app ck-auto-demo --env prod --build_number 92 --flow terminate-cluster --template payer --payer '674600239845,741843927392' --core_node_spot_percent 80
 
-#     """,
-#     dag=dag,
-# )
+    """,
+    dag=dag,
+)
 master_refresh = BashOperator(
     task_id='master_refresh',
     bash_command=f"""pwd
@@ -143,9 +143,9 @@ master_refresh = BashOperator(
                      cd mycloud-scripts
                      ls
                      git checkout ck-payer-analytics-snowflake-demo
-                     python3 ck_auto_emr_demo/main.py --year 2023 --month 10 --env 'prod' --payer '674600239845,741843927392' --build_number '91'
+                     python3 ck_auto_emr_demo/main.py --year 2023 --month 10 --env 'prod' --payer '674600239845,741843927392' --build_number '92'
                    
     """,
     dag=dag,
 )
-#  build_jar >> ri_config >> refresh_data >> emr_process
+ build_jar >> ri_config >> refresh_data >> emr_process >> master_refresh
